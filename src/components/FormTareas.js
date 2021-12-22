884b35cc90cc29854a85dbeb6108217b3703a5ad
Emilse <emilsearias@gmail.com>
2021-12-22 11:35:54 -0300

    agregamos el state arregloTareas

--- src/components/FormTareas.js
+++ src/components/FormTareas.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 // import Form from 'react-bootstrap/Form'
 // import Button from 'react-bootstrap/Button'
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 
 const FormTareas = () => {
+    //aqui va la logica
+    const [arregloTareas, setArregloTareas] = useState([]);
+
+
   return (
     <div>
       <Form>
